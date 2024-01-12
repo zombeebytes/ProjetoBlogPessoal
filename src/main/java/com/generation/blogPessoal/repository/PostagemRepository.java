ec0def4d2cfa2c872aca5c10389532697fb97a9f
unknown <andressapatricio93@gmail.com>
2024-01-12 19:43:35 -0300

    Blog Pessoal

--- src/main/java/com/generation/blogPessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogPessoal/repository/PostagemRepository.java
@@ -1,9 +1,15 @@
 package com.generation.blogPessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.generation.blogPessoal.model.Postagem;
 //PDA dá o poder para o repository se comunicar com o banco de dados
+@Repository
 public interface PostagemRepository extends JpaRepository <Postagem, Long> {
-
+		List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+		
 }
